4b628e054375a34a7d3ee9c2992e9b69773bccbf
sexytoaster <paddyoconnor1@hotmail.com>
2020-04-21 17:57:57 +0100

    first commit
    started spawning cylanders

---
+++ Assets/Scripts/SpawnTrafficLights.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrafficLights : MonoBehaviour
+{
+    public float lightCount;
+    public float radius;
+    public GameObject trafficLight;
+    private Vector3 centrePos = new Vector3 (0, 0, 0);
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        InstantiateLights();
+    }
+    
+    void InstantiateLights()
+    {
+        float angle = 360f / lightCount;
+        for(int i = 0; i < lightCount; i++)
+        {
+            Quaternion rotation = Quaternion.AngleAxis(i * angle, Vector3.up);
+            Vector3 direction = rotation * Vector3.forward;
+
+            Vector3 position = centrePos + (direction * radius);
+            Instantiate(trafficLight, position, rotation);
+        }
+    }
+}
